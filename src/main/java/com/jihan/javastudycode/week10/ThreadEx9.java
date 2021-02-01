62aef71056f6557e306ec14fef3b4085b44d7ce7
rowanlee92 <watrv41@gmail.com>
2021-02-01 19:59:49 +0900

    Update: week_10

---
+++ src/main/java/com/jihan/javastudycode/week10/ThreadEx9.java
@@ -0,0 +1,28 @@
+package com.jihan.javastudycode.week10;
+
+public class ThreadEx9 {
+    public static void main(String[] args) {
+        ThreadGroup main = Thread.currentThread().getThreadGroup();
+        ThreadGroup grp1 = new ThreadGroup("Group1");
+        ThreadGroup grp2 = new ThreadGroup("Group2");
+
+        // ThreadGroup(ThreadGroup parent, String name)
+        ThreadGroup subGrp1 = new ThreadGroup(grp1, "SubGroup1");
+
+        grp1.setMaxPriority(3); // 쓰레드 그룹 grp1의 최대우선순위를 3으로 변경.
+
+        Runnable r = () -> {
+            try {
+                Thread.sleep(1000L);    // 쓰레드를 1초간 멈춤.
+            } catch (InterruptedException e) {}
+        };
+
+        new Thread(grp1, r, "th1").start();
+        new Thread(subGrp1, r, "th2").start();
+        new Thread(grp2, r, "th3").start();
+
+        System.out.println(">>List of ThreadGroup : " +  main.getName() + ", Active ThreadGroup: " + main.activeGroupCount()
+        + ", Active Thread: " + main.activeCount());
+        main.list();
+    }
+}
