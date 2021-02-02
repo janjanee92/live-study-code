90b9e1a31ca0949a867fb4c463b4da009a555137
rowanlee92 <watrv41@gmail.com>
2021-02-02 16:25:32 +0900

    Update: week_10

---
+++ src/main/java/com/jihan/javastudycode/week10/ThreadEx19.java
@@ -0,0 +1,42 @@
+package com.jihan.javastudycode.week10;
+
+public class ThreadEx19 {
+
+    static long startTime = 0L;
+
+    public static void main(String[] args) {
+        ThreadEx19_1 th1 = new ThreadEx19_1();
+        ThreadEx19_2 th2 = new ThreadEx19_2();
+
+        th1.start();
+        th2.start();
+
+        startTime = System.currentTimeMillis();
+
+        try {
+            th1.join();
+            th2.join();
+        } catch (InterruptedException e) {}
+
+        System.out.print("소요시간: " + (System.currentTimeMillis() - ThreadEx19.startTime));
+    }
+
+}
+
+class ThreadEx19_1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 300; i++) {
+            System.out.print(new String("-"));
+        }
+    }
+}
+class ThreadEx19_2 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 300; i++) {
+            System.out.print(new String("|"));
+        }
+    }
+}
+
