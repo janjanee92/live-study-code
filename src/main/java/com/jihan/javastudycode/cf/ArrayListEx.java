5098a7f94875ecfe605b18d01eb7e7b096fde4a9
rowanlee92 <watrv41@gmail.com>
2021-06-21 22:40:09 +0900

    Add: ArrayListEx, MyVector 구현

---
+++ src/main/java/com/jihan/javastudycode/cf/ArrayListEx.java
@@ -0,0 +1,27 @@
+package com.jihan.javastudycode.cf;
+
+import java.util.*;
+
+public class ArrayListEx {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("cat");
+        list.add("hi");
+        list.add("dog");
+        list.add("good");
+        list.add("friends");
+
+        System.out.println(list.size());   //  6
+        System.out.println(list.get(3));   //  dog
+        list.remove(3);
+        list.remove("cat");
+        System.out.println(list);   //  [hello, hi, good, friends]
+
+        List<String> companies = Arrays.asList("google", "apple", "samsung");
+        System.out.println(companies);  //  [google, apple, samsung]
+
+        List<Integer> numbers = Arrays.asList(1, 10, 100);
+        System.out.println(numbers);  //  [1, 10, 100]
+    }
+}
