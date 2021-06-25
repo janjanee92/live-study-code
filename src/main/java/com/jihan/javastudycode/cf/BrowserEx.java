1f2b50aa9bb0c574df1a508db714074565cfa599
rowanlee92 <watrv41@gmail.com>
2021-06-25 23:48:39 +0900

    Add: Browser, ExpValidCheck - StackNQueue

---
+++ src/main/java/com/jihan/javastudycode/cf/BrowserEx.java
@@ -0,0 +1,58 @@
+package com.jihan.javastudycode.cf;
+
+import java.util.Stack;
+
+public class BrowserEx {
+
+    public static Stack<String> back = new Stack<>();
+    public static Stack<String> forward = new Stack<>();
+
+    public static void main(String[] args) {
+        goURL("1.구글");
+        goURL("2.네이버");
+        goURL("3.넷플릭스");
+        goURL("4.왓챠");
+
+        printStatus();
+
+        goBack();
+        System.out.println("===== '뒤로' 버튼을 누른 후 =====");
+        printStatus();
+
+        goBack();
+        System.out.println("===== '뒤로' 버튼을 누른 후 =====");
+        printStatus();
+
+        goForward();
+        System.out.println("===== '앞으로' 버튼을 누른 후 =====");
+        printStatus();
+
+        goURL("5. 유투브");
+        System.out.println("===== 새로운 주소로 이동 ====");
+        printStatus();
+    }
+
+    public static void printStatus() {
+        System.out.println("back: " + back);
+        System.out.println("forward: " + forward);
+        System.out.println("현재화면 '" + back.peek() + "'");
+        System.out.println();
+    }
+
+    public static void goURL(String url) {
+        back.push(url);
+        if(!forward.empty())
+            forward.clear();
+    }
+
+    public static void goBack() {
+        if (!back.empty())
+            forward.push(back.pop());
+    }
+
+    public static void goForward() {
+        if (!forward.empty())
+            back.push(forward.pop());
+    }
+
+}
---
+++ src/main/java/com/jihan/javastudycode/cf/ExpValidCheck.java
@@ -0,0 +1,38 @@
+package com.jihan.javastudycode.cf;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class ExpValidCheck {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage : java ExpValidCheck \"EXPRESSION\"");
+            System.out.println("Example : java ExpVAlidCheck \"((2+3)*1)+3\"");
+            System.exit(0);
+        }
+
+        Stack<Character> st = new Stack<>();
+        String expression = args[0];
+
+        System.out.println("expression: " + expression);
+
+        try {
+            for (int i = 0; i < expression.length(); i++) {
+                char ch = expression.charAt(i);
+
+                if (ch == '(') {
+                    st.push(ch);
+                } else if (ch == ')') {
+                    st.pop();
+                }
+            }
+            if (st.isEmpty()) {
+                System.out.println("괄호 일치");
+            } else {
+                System.out.println("괄호 불일치");
+            }
+        } catch (EmptyStackException e) {
+            System.out.println("괄호 불일치");
+        }
+    }
+}
