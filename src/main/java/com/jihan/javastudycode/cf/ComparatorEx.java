a564fdb63104f174f2a05b71dbdde31b523f6d5c
rowanlee92 <watrv41@gmail.com>
2021-06-29 00:13:13 +0900

    Add: Comparator, Comparable Example

--- src/main/java/com/jihan/javastudycode/cf/ComparatorEx.java
+++ src/main/java/com/jihan/javastudycode/cf/ComparatorEx.java
@@ -27,7 +27,7 @@ public class ComparatorEx {
 class Descending implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        return o1.compareTo(o2);
+        return o2.compareTo(o1);
     }
 }
 
